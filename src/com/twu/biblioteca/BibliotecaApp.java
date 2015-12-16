58469cd92edacafce75ddc390b4d833c175e2e2f
Zhang Wei <lzhangwei@outlook.com>
2015-12-16 22:23:10 +0800

    add print welcome message function

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,17 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.printWelcomeMessage();
+    }
+
+    private void printWelcomeMessage() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("*********************************************\n");
+        stringBuilder.append("*                                           *\n");
+        stringBuilder.append("*            Welcome Biblioteca!            *\n");
+        stringBuilder.append("*                                           *\n");
+        stringBuilder.append("*********************************************\n");
+        System.out.println(stringBuilder);
     }
 }
