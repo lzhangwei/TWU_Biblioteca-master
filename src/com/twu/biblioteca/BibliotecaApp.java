df653e620ac89b19afabae22050598b642078fa8
Zhang Wei <lzhangwei@outlook.com>
2015-12-27 22:00:49 +0800

    add SelectOptions

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 public class BibliotecaApp {
 
-    private List<Book> bookList = new ArrayList<>();
+    {
+        createBooks();
+    }
+
+    private List<Book> bookList;
 
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
@@ -13,10 +17,11 @@ public class BibliotecaApp {
 
         bibliotecaApp.printBookListTitle();
 
-        bibliotecaApp.createBooks();
         bibliotecaApp.bookList.forEach(book -> {
             printBookInfo(book);
         });
+
+        bibliotecaApp.printSelectOptions();
     }
 
     private void printWelcomeMessage() {
@@ -38,7 +43,19 @@ public class BibliotecaApp {
         System.out.println(stringBuilder);
     }
 
+    private void printSelectOptions() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n");
+        stringBuilder.append("1. If you want list books,please enter `LIST-BOOKS`!\n");
+        stringBuilder.append("2. If you want check out a book,please enter `CHECK-OUT`!\n");
+        stringBuilder.append("3. If you want return book,please enter `RETURN`!\n");
+        stringBuilder.append("4. If you want quit,please enter `QUIT`!\n");
+        stringBuilder.append("\n");
+        System.out.println(stringBuilder);
+    }
+
     private void createBooks() {
+        bookList = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             bookList.add(
                     BookBuilder.aBook()
@@ -48,7 +65,6 @@ public class BibliotecaApp {
                             .build());
         }
     }
-
 
     private static void printBookInfo(Book book) {
         System.out.println(String.format("     %s   %s   %s", book.getName(), book.getAuthor(), book.getYearPublished()));
