32079253c70d375e35b673d18d1abb2711125220
Zhang Wei <lzhangwei@outlook.com>
2016-01-13 22:46:02 +0800

    add login method

--- src/com/twu/biblioteca/account/Account.java
+++ src/com/twu/biblioteca/account/Account.java
@@ -1,8 +1,16 @@
 package com.twu.biblioteca.account;
 
+import com.twu.biblioteca.book.Book;
+import com.twu.biblioteca.movie.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
     private User user;
     private String password;
+    private List<Book> checkoutBooks = new ArrayList<>();
+    private List<Movie> checkoutMovies = new ArrayList<>();
 
     public Account(User user, String password) {
         this.user = user;
@@ -23,5 +31,21 @@ public class Account {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Book> getCheckoutBooks() {
+        return checkoutBooks;
+    }
+
+    public void setCheckoutBooks(List<Book> checkoutBooks) {
+        this.checkoutBooks = checkoutBooks;
+    }
+
+    public List<Movie> getCheckoutMovies() {
+        return checkoutMovies;
+    }
+
+    public void setCheckoutMovies(List<Movie> checkoutMovies) {
+        this.checkoutMovies = checkoutMovies;
     }
 }
--- src/com/twu/biblioteca/account/AccountController.java
+++ src/com/twu/biblioteca/account/AccountController.java
@@ -1,4 +1,36 @@
 package com.twu.biblioteca.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountController {
+    {
+        createRegistedAccount();
+    }
+
+    private List<Account> registeredAccountList = new ArrayList<>();
+
+    private Account loginAccount;
+
+    private void createRegistedAccount() {
+        for (int i = 0; i < 10; i++) {
+            registeredAccountList.add(new Account(
+                    User.UserBuilder.anUser().withName("USER" + i)
+                            .withEmail("EMAIL" + i)
+                            .withPhone("PHONE" + i)
+                            .withLibraryNumber("111-111" + i)
+                            .build(), "password" + i
+            ));
+        }
+    }
+
+    public String login(String libraryNumber, String password) {
+        for (Account account : registeredAccountList) {
+            if (libraryNumber.equals(account.getUser().getLibraryNumber()) && password.equals(account.getPassword())) {
+                loginAccount = account;
+                return "Login Success";
+            }
+        }
+        return "Login Error";
+    }
 }
